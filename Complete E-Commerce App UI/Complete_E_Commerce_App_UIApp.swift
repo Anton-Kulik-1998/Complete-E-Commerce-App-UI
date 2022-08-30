a33aa5368fc52588c91fa1791015867689d82484
Антон Кулик <anton.smile.1998@gmail.com>
2022-08-30 22:40:43 +0300

    Initial Commit

---
+++ Complete E-Commerce App UI/Complete_E_Commerce_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Complete_E_Commerce_App_UIApp.swift
+//  Complete E-Commerce App UI
+//
+//  Created by Антон Кулик on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Complete_E_Commerce_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Complete E-Commerce App UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Complete E-Commerce App UI
+//
+//  Created by Антон Кулик on 30.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
